24c25b84e790c1dbf2eddaa439fead9648d2d74e
Jehu Casimiro <jehubaclecasimiro@gmail.com>
2026-06-27 00:02:37 +0800

    Update slider.tsx

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,14 +1,14 @@
-import * as React from "react"
-import { Slider as SliderPrimitive } from "radix-ui"
+import * as React from 'react';
+import { Slider as SliderPrimitive } from 'radix-ui';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 type SliderProps = React.ComponentProps<typeof SliderPrimitive.Root> & {
-  trackClassName?: string
-  rangeClassName?: string
-  thumbClassName?: string
-  trackStyle?: React.CSSProperties
-}
+  trackClassName?: string;
+  rangeClassName?: string;
+  thumbClassName?: string;
+  trackStyle?: React.CSSProperties;
+};
 
 function Slider({
   className,
@@ -29,8 +29,8 @@ function Slider({
         : Array.isArray(defaultValue)
           ? defaultValue
           : [min, max],
-    [value, defaultValue, min, max]
-  )
+    [value, defaultValue, min, max],
+  );
 
   return (
     <SliderPrimitive.Root
@@ -40,8 +40,8 @@ function Slider({
       min={min}
       max={max}
       className={cn(
-        "relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col",
-        className
+        'relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col',
+        className,
       )}
       {...props}
     >
@@ -49,15 +49,15 @@ function Slider({
         data-slot="slider-track"
         style={trackStyle}
         className={cn(
-          "relative grow overflow-hidden rounded-[3px] bg-muted data-horizontal:h-1.5 data-horizontal:w-full data-vertical:h-full data-vertical:w-1.5",
-          trackClassName
+          'relative grow overflow-hidden rounded-sm bg-muted data-horizontal:h-1.5 data-horizontal:w-full data-vertical:h-full data-vertical:w-1.5',
+          trackClassName,
         )}
       >
         <SliderPrimitive.Range
           data-slot="slider-range"
           className={cn(
-            "absolute bg-primary select-none data-horizontal:h-full data-vertical:w-full",
-            rangeClassName
+            'absolute bg-primary select-none data-horizontal:h-full data-vertical:w-full',
+            rangeClassName,
           )}
         />
       </SliderPrimitive.Track>
@@ -66,13 +66,13 @@ function Slider({
           data-slot="slider-thumb"
           key={index}
           className={cn(
-            "relative block size-3.5 shrink-0 rounded-[3px] border border-ring bg-white select-none after:absolute after:-inset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
-            thumbClassName
+            'relative block size-3.5 shrink-0 rounded-sm border border-ring bg-white select-none after:absolute after:-inset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer',
+            thumbClassName,
           )}
         />
       ))}
     </SliderPrimitive.Root>
-  )
+  );
 }
 
-export { Slider }
+export { Slider };
